36bf5ca9d3afc81058c5ef71e6f8d8b03e9ff0b3
Lê Vũ Ngọc Huy <lvnhuy1406@gmail.com>
2023-11-07 20:43:23 +0700

    PaymentController Update

--- src/main/java/com/example/onlinepay/controller/PaymentController.java
+++ src/main/java/com/example/onlinepay/controller/PaymentController.java
@@ -20,29 +20,33 @@ import java.util.*;
 @RequestMapping("/api/payment")
 public class PaymentController {
     @GetMapping("/create_payment")
-    public ResponseEntity<?> createPayment() throws UnsupportedEncodingException {
-//        String orderType = "other";
-//        long amount = Integer.parseInt(req.getParameter("amount"))*100;
-//        String bankCode = req.getParameter("bankCode");
-
-        long amount = 1000000;
+    public String createPayment() throws UnsupportedEncodingException {
+        String vnp_Version = "2.1.0";
+        String vnp_Command = "pay";
+        String orderType = "other";
+        long amount = 1500000*100;
+        String bankCode = "NCB";
 
         String vnp_TxnRef = Config.getRandomNumber(8);
-//        String vnp_IpAddr = Config.getIpAddress(req);
+        String vnp_IpAddr = "127.0.0.1";
 
         String vnp_TmnCode = Config.vnp_TmnCode;
 
         Map<String, String> vnp_Params = new HashMap<>();
-        vnp_Params.put("vnp_Version", Config.vnp_Version);
-        vnp_Params.put("vnp_Command", Config.vnp_Command);
+        vnp_Params.put("vnp_Version", vnp_Version);
+        vnp_Params.put("vnp_Command", vnp_Command);
         vnp_Params.put("vnp_TmnCode", vnp_TmnCode);
         vnp_Params.put("vnp_Amount", String.valueOf(amount));
         vnp_Params.put("vnp_CurrCode", "VND");
-        vnp_Params.put("vnp_BankCode", "NCB");
-        vnp_Params.put("vnp_Locale", "vn");
+
+        vnp_Params.put("vnp_BankCode", bankCode);
         vnp_Params.put("vnp_TxnRef", vnp_TxnRef);
         vnp_Params.put("vnp_OrderInfo", "Thanh toan don hang:" + vnp_TxnRef);
+        vnp_Params.put("vnp_OrderType", orderType);
+
+        vnp_Params.put("vnp_Locale", "vn");
         vnp_Params.put("vnp_ReturnUrl", Config.vnp_ReturnUrl);
+        vnp_Params.put("vnp_IpAddr", vnp_IpAddr);
 
         Calendar cld = Calendar.getInstance(TimeZone.getTimeZone("Etc/GMT+7"));
         SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss");
@@ -81,13 +85,77 @@ public class PaymentController {
         queryUrl += "&vnp_SecureHash=" + vnp_SecureHash;
         String paymentUrl = Config.vnp_PayUrl + "?" + queryUrl;
 
-        PaymentRestDTO paymentRestDTO = new PaymentRestDTO();
-        paymentRestDTO.setStatus("OK");
-        paymentRestDTO.setMessage("Successfully");
-        paymentRestDTO.setURL(paymentUrl);
-
-        return ResponseEntity.status(HttpStatus.OK).body(paymentRestDTO);
+        return paymentUrl;
     }
+//    public ResponseEntity<?> createPayment() throws UnsupportedEncodingException {
+////        String orderType = "other";
+////        long amount = Integer.parseInt(req.getParameter("amount"))*100;
+////        String bankCode = req.getParameter("bankCode");
+//
+//        long amount = 1000000;
+//
+//        String vnp_TxnRef = Config.getRandomNumber(8);
+////        String vnp_IpAddr = Config.getIpAddress(req);
+//
+//        String vnp_TmnCode = Config.vnp_TmnCode;
+//
+//        Map<String, String> vnp_Params = new HashMap<>();
+//        vnp_Params.put("vnp_Version", Config.vnp_Version);
+//        vnp_Params.put("vnp_Command", Config.vnp_Command);
+//        vnp_Params.put("vnp_TmnCode", vnp_TmnCode);
+//        vnp_Params.put("vnp_Amount", String.valueOf(amount));
+//        vnp_Params.put("vnp_CurrCode", "VND");
+//        vnp_Params.put("vnp_BankCode", "NCB");
+//        vnp_Params.put("vnp_Locale", "vn");
+//        vnp_Params.put("vnp_TxnRef", vnp_TxnRef);
+//        vnp_Params.put("vnp_OrderInfo", "Thanh toan don hang:" + vnp_TxnRef);
+//        vnp_Params.put("vnp_ReturnUrl", Config.vnp_ReturnUrl);
+//        vnp_Params.put("vnp_IpAddr", "192.168.1.8");
+//
+//        Calendar cld = Calendar.getInstance(TimeZone.getTimeZone("Etc/GMT+7"));
+//        SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss");
+//        String vnp_CreateDate = formatter.format(cld.getTime());
+//        vnp_Params.put("vnp_CreateDate", vnp_CreateDate);
+//
+//        cld.add(Calendar.MINUTE, 15);
+//        String vnp_ExpireDate = formatter.format(cld.getTime());
+//        vnp_Params.put("vnp_ExpireDate", vnp_ExpireDate);
+//
+//        List fieldNames = new ArrayList(vnp_Params.keySet());
+//        Collections.sort(fieldNames);
+//        StringBuilder hashData = new StringBuilder();
+//        StringBuilder query = new StringBuilder();
+//        Iterator itr = fieldNames.iterator();
+//        while (itr.hasNext()) {
+//            String fieldName = (String) itr.next();
+//            String fieldValue = (String) vnp_Params.get(fieldName);
+//            if ((fieldValue != null) && (fieldValue.length() > 0)) {
+//                //Build hash data
+//                hashData.append(fieldName);
+//                hashData.append('=');
+//                hashData.append(URLEncoder.encode(fieldValue, StandardCharsets.US_ASCII.toString()));
+//                //Build query
+//                query.append(URLEncoder.encode(fieldName, StandardCharsets.US_ASCII.toString()));
+//                query.append('=');
+//                query.append(URLEncoder.encode(fieldValue, StandardCharsets.US_ASCII.toString()));
+//                if (itr.hasNext()) {
+//                    query.append('&');
+//                    hashData.append('&');
+//                }
+//            }
+//        }
+//        String queryUrl = query.toString();
+//        String vnp_SecureHash = Config.hmacSHA512(Config.secretKey, hashData.toString());
+//        queryUrl += "&vnp_SecureHash=" + vnp_SecureHash;
+//        String paymentUrl = Config.vnp_PayUrl + "?" + queryUrl;
+//
+//        PaymentRestDTO paymentRestDTO = new PaymentRestDTO();
+//        paymentRestDTO.setStatus("OK");
+//        paymentRestDTO.setMessage("Successfully");
+//        paymentRestDTO.setURL(paymentUrl);
+//
+//        return ResponseEntity.status(HttpStatus.OK).body(paymentRestDTO);
+//    }
 
     @GetMapping("/payment_info")
     public ResponseEntity<?> transaction(
